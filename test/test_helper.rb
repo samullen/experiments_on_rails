ffee014e0e8eb98395864d1f7ea9541883ed6e33
Samuel Mullen <samuel@samuelmullen.com>
2015-02-17 07:23:16 -0600

    setup initial test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,23 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/rails/capybara"
+
+include FactoryGirl::Syntax::Methods
+Dir[Rails.root.join("test/support/**/*.rb")].each {|f| require f}
+
+class MiniTest::Spec
+  before :each do
+    DatabaseCleaner.start
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
+end
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  self.use_transactional_fixtures = true
 end
