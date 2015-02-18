bbe941f9f50eac57ff37cdf2a599e7ccb0ac7f09
Samuel Mullen <samuel@samuelmullen.com>
2015-02-18 06:47:44 -0600

    Add feature test for the landing page

---
+++ test/features/landing_page_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+feature "Landing page" do
+  scenario "Successful display" do
+    visit root_path
+
+    page.all("div.search_result-thumbnail").size.must_equal 5
+  end
+end
