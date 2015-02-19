9eeeeed63ebf3bc0862c09e4b7c73c21b2ba0ac0
Samuel Mullen <samuel@samuelmullen.com>
2015-02-19 06:55:55 -0600

    finish initial landing page feature test

--- test/models/landing_page_test.rb
+++ test/models/landing_page_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-describe LandingPage do
+class LandingPageTest < ActiveSupport::TestCase
   describe "#products" do
     it "returns no more than 10 products" do
       LandingPage.new.products.size.must_equal 10
