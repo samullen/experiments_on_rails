5ead9e5dcb7cab406388c9e5eb4da820df258b87
Samuel Mullen <samuel@samuelmullen.com>
2015-03-09 07:20:50 -0500

    look for products within #products-promoted

--- test/features/first_page_test.rb
+++ test/features/first_page_test.rb
@@ -4,6 +4,8 @@ feature "Visiting the site through the first page" do
   scenario "Happy path" do
     visit root_path
 
-    page.all("div.search_result-thumbnail").size.must_equal 10
+    within "#products-promoted" do
+      page.all("div.search_result-thumbnail").size.must_equal 10
+    end
   end
 end
